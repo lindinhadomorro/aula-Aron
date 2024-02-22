1207364e49e749f658dd410d33bae30179ad5539
lindinhadomorro <132684870+lindinhadomorro@users.noreply.github.com>
2024-02-22 08:54:52 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`Conteúdo inicial da variavel: ${pessoa} | Tipo ${typeof(pessoa)}`);
+
+pessoa = 259;
+
+console.log(`Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`);
