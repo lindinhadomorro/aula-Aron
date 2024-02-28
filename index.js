6d8bfbcd9bbdf7664f86537a00463ca72f6242c8
lindinhadomorro <132684870+lindinhadomorro@users.noreply.github.com>
2024-02-28 09:48:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-var pessoa = "832.192.304-34"
+// O que são funções no JavaScript?
+// Função são basicamente campos no código que tem alguma funcionalidade
+// Um exemplo é, você quer fazer funções de calculos para  uma calculadora
+// Cada operação precisa de uma função.
 
-console.log(`Conteúdo inicial da variavel: ${pessoa} | Tipo ${typeof(pessoa)}`);
+//Exemplo de função:
+function somar(){
+    const num1 = 10;
+    const num2 = 20;
 
-pessoa = 259;
+    const total = num1 + num2;
 
-console.log(`Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`);
+    console.log(`A soma ${num1} + ${num2} = ${total}`);
+}
+
+// Para podermos utilizar uma função no Javascript, precisamos chamar essa função
+// No código
+ 
+somar(); // Chamamos a função para executar o código acima
+ 
+
+ // Nós temos 2 tipos de função, com parametros e sem parametros
+
+ // Com parametros
+ function somar2(num1, num2){
+    const total = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${total}`);
+}
+
+
+// Sem parametro
+function somar3(){
+    const num1 = 10;
+    const num2 = 20;
+
+    const total = num1 + num2;
+
+    console.log(`A soma ${num1} + ${num2} = ${total}`);
+}
